ef0fac85e4ef93b3e92b6cc43c6d3e162e2a63be
Suha Karabey <suhakarabey@kodlaa.com>
2020-04-06 11:44:52 +0300

    ExpandapleCard showcase view

--- lib/components/expandablecardpage.dart
+++ lib/components/expandablecardpage.dart
@@ -1,19 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:showcaseview/showcase.dart';
 
 import 'expandablecard.dart';
 
 class ExpandableCardPage extends StatelessWidget {
-  ExpandableCardPage({@required this.page, @required this.expandableCard});
+  ExpandableCardPage(
+      {@required this.page, @required this.expandableCard, this.key});
 
   final Widget page;
   final ExpandableCard expandableCard;
+  final GlobalKey key;
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
         page,
-        expandableCard
+        Showcase(
+            key: key,
+            description: 'Tap to see menu options',
+            disableAnimation: true,
+            child: expandableCard)
       ],
     );
   }
